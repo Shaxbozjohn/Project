71dd0b2aa92c73b4653fd244cbcf1942479bd3e4
Ibragimov Shaxboz <133381675+Shaxbozjohn@users.noreply.github.com>
2023-05-17 20:26:09 +0500

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,17 @@
+num = int(input(f"Nechta xodim kelmadi? "))
+
+names = [ ]
+
+for i in range(1, num+1):
+
+
+ names.append(input(f"{i} - xodimning ismini kiriting: ").title())
+
+print("{','.join(names)} - xodimlarga xabar yuborildi ")
+
+
+
+
+
+
+ 
